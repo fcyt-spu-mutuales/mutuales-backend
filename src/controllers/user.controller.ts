ed21a6dcdc3290e0ce1ab859d528ad3fe4697f18
NestorFlores <n.flores@spds.info>
2019-02-16 18:12:54 -0300

    Added controller example

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,60 @@
+import {
+    Param,
+    Body,
+    Get,
+    Post,
+    Put,
+    Delete,
+    JsonController
+  } from "routing-controllers";
+  import { getManager } from "typeorm";
+  import { User } from "../persistence/entity/user.entity";
+  
+  @JsonController()
+  export class UserController {
+    repository: any;
+    constructor() {
+      this.repository = getManager().getRepository(User);
+    }
+    @Get("/users")
+    getAll() {
+      return this.repository.find();
+    }
+  
+    @Get("/users/:id")
+    getOne(@Param("id") id: number) {
+      return this.repository.IndOne(id);
+    }
+  
+    @Post("/users/create")
+    async post(@Body() request: any) {
+      console.log(request)
+      const newUser = new User();
+      newUser.password = request.password;
+      newUser.email = request.email;
+      newUser.firstName = request.firstName;
+      newUser.lastName = request.lastName;
+  
+      const userToSave: User = await this.repository.create(newUser);
+      return this.repository.save(userToSave);
+    }
+  
+    @Put("/users/:id")
+    put(@Param("id") id: number, @Body() user: User) {
+      const currentUser: User = this.repository.findOne(id);
+  
+      currentUser.email = user.email;
+      currentUser.firstName = user.firstName;
+      currentUser.password = user.password;
+  
+      return this.repository.save(currentUser);
+    }
+  
+    @Delete("/users/:id")
+    remove(@Param("id") id: number) {
+      const currentUser: User = this.repository.findOne(id);
+      if (currentUser) {
+        return this.repository.remove(currentUser);
+      }
+    }
+  }
---
+++ src/persistence/entity/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  OneToMany,
+  ManyToOne
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({
+    unique: true
+  })
+  email: string;
+
+  @Column()
+  password: string;
+}
