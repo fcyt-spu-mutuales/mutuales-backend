6d28f637fcb7ce4b61eba2c2017ffd724a46f078
Nestor Flores <nestor.flores@flexdigitalhelath.com>
2019-02-21 10:42:16 -0300

    Added exception handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,81 +1,81 @@
-import {
-    Param,
-    Body,
-    Get,
-    Post,
-    Put,
-    Delete,
-    JsonController
-  } from "routing-controllers";
-  import { getManager } from "typeorm";
-  import { User } from "../persistence/entity/user.entity";
-  import bcrypt = require('bcrypt');
+import { Param, Body, Get, Post, Put, Delete, JsonController } from 'routing-controllers';
+import { getManager } from 'typeorm';
+import { User } from '../persistence/entity/user.entity';
+import bcrypt = require('bcrypt');
 
+@JsonController()
+export class UserController {
+  repository: any;
+  constructor() {
+    this.repository = getManager().getRepository(User);
+  }
+  @Get('/users')
+  getAll() {
+    return this.repository.find();
+  }
 
-  @JsonController()
-  export class UserController {
-    repository: any;
-    constructor() {
-      this.repository = getManager().getRepository(User);
-    }
-    @Get("/users")
-    getAll() {
-      return this.repository.find();
-    }
-  
-    @Get("/users/:id")
-    getOne(@Param("id") id: number) {
-      return this.repository.findOne(id);
-    }
-  
-    @Post("/users/create")
-    async post(@Body() request: any) {
-    
-      const newUser = {
-        ...request,
-        password: bcrypt.hashSync(request.password, 10)
+  @Get('/users/:id')
+  getOne(@Param('id') id: number) {
+    return this.repository.findOne(id);
+  }
+
+  @Post('/users/create')
+  async post(@Body() request: any) {
+    const newUser = {
+      ...request,
+      password: bcrypt.hashSync(request.password, 10)
+    };
+    try {
+      const userToSave: User = await this.repository.save(newUser)
+      return {
+        sucess: true,
+        id: userToSave.id,
+        message: 'User Created'
+      }
+    } catch (error) {
+      return {
+        success: false,
+        message: error
       };
-    
-      const userToSave: User = await this.repository.create(newUser)
-      return this.repository.save(userToSave)
-    }
-  
-    @Post("/users/login")
-    async login(@Body() request: any) {
-
-      const userToFind = await this.repository.createQueryBuilder("user")
-                                              .where("user.email = :email", {email: request.email})
-                                              .getOne();
-      
-      // If user doesn't exist just return a message 
-      if(!userToFind) {
-        return {
-          sucess: false,
-          message: "Invalid user or password"
-        }
-      }
-
-      // Validate password
-      if (bcrypt.compareSync(request.password, userToFind.password)) {
-          return {
-            id: userToFind.id,
-            firstName: userToFind.firstName,
-            lastName: userToFind.lastName,
-            success: true
-          }
-      } else {
-        return {
-          success: false,
-          message: "Invalid user or password"
-        }
-      }
-    }
-  
-    @Delete("/users/:id")
-    remove(@Param("id") id: number) {
-      const currentUser: User = this.repository.findOne(id);
-      if (currentUser) {
-        return this.repository.remove(currentUser);
-      }
     }
   }
+
+  @Post('/users/login')
+  async login(@Body() request: any) {
+    const userToFind = await this.repository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email: request.email })
+      .getOne();
+
+    // If user doesn't exist just return a message
+    if (!userToFind) {
+      return {
+        sucess: false,
+        message: 'Invalid user or password'
+      };
+    }
+
+    // Validate password
+    if (bcrypt.compareSync(request.password, userToFind.password)) {
+      return {
+        id: userToFind.id,
+        firstName: userToFind.firstName,
+        lastName: userToFind.lastName,
+        success: true
+      };
+    } else {
+      return {
+        success: false,
+        message: 'Invalid user or password'
+      };
+    }
+  }
+
+  @Delete('/users/:id')
+  remove(@Param('id') id: number) {
+    const currentUser: User = this.repository.findOne(id);
+    if (currentUser) {
+      return this.repository.remove(currentUser);
+    }
+  }
+}
